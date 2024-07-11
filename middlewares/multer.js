7e9dd50649a44a97ab5ef598b3cbf831e13f091e
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-07-11 18:43:48 -0400

    Setup cloudinary for uploading images

---
+++ middlewares/multer.js
@@ -0,0 +1,23 @@
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import cloudinary from "../config/cloudinary.js";
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "travel_stories",
+    format: async (req, file) => "jpg",
+    public_id: (req, file) => file.originalname.split(".")[0],
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// This code is for testing purpose, needs to be removed later.
+upload.single("image"),
+  (req, res, next) => {
+    console.log(req.file.path);
+    next();
+  };
+
+export default upload;
